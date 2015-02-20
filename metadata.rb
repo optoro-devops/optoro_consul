12f033e068146abb245eb0e0eae056d904fca7b0
Scott Medefind <smedefind@optoro.com>
2015-02-19 23:50:22 -0500

    Upgrading to consul 0.5

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer 'devops@optoro.com'
 license 'MIT'
 description 'Installs Consul'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.3'
+version '0.0.4'
 
 depends 'consul', '~> 0.7.1'
