5ee3db3dd96a7d39a8c14c534ce98d32aec6545a
Scott Medefind <smedefind@optoro.com>
2015-09-02 14:12:32 -0400

    Adding client recipe and documentation

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,15 @@ maintainer_email 'devops@optoro.com'
 license 'MIT'
 description 'Installs Consul'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.5'
+version '0.0.6'
+
+supports 'ubuntu', '= 14.04'
+
+provides 'optoro_consul::default'
+provides 'optoro_consul::client'
+
+recipe 'optoro_consul::default', 'Installs consul server'
+recipe 'optoro_consul::client', 'Installs consul client'
 
 depends 'consul', '~> 0.7.1'
 depends 'optoro_nrpechecks'
---
+++ spec/client_spec.rb
@@ -0,0 +1,20 @@
+describe 'optoro_consul::client' do
+  Resources::PLATFORMS.each do |platform, value|
+    value['versions'].each do |version|
+      context "On #{platform} #{version}" do
+        include_context 'optoro_consul'
+
+        let(:chef_run) do
+          ChefSpec::SoloRunner.new(platform: platform, version: version, log_level: :error) do |node|
+            node.set['lsb']['codename'] = value['codename']
+          end.converge(described_recipe)
+        end
+
+        it 'Include consul::default' do
+          expect(chef_run).to include_recipe('consul::default')
+        end
+
+      end
+    end
+  end
+end
