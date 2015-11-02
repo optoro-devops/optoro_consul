877fcbb1b7ca3ea04c6d14276c9665e9bd115d25
Jeff Gillis <jgillis@optoro.com>
2015-11-02 13:50:26 -0500

    Make client agent registration work

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,3 +1,5 @@
 node.override['consul']['service_mode'] = 'client'
 node.default['consul']['serve_ui'] = false
+node.default['consul']['service']['server'] = false
+node.default['consul']['service']['expect_bootstrap'] = 0
 include_recipe 'consul'
