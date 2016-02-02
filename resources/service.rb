5344b4378690985f113fa746adafa1ffe6846198
Jeff Gillis <jgillis@optoro.com>
2016-02-02 13:05:44 -0500

    Allow arbitrary service attributes
    Update Thorfile
    Use overcommit

--- resources/service.rb
+++ resources/service.rb
@@ -4,4 +4,4 @@ default_action :create
 
 attribute :service_name, kind_of: String, name_attribute: true
 attribute :port, kind_of: Integer
-attribute :checks, kind_of: Array, default: []
+attribute :params, kind_of: Hash, default: {}
