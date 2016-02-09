af2009b558bd00ac163e4c1ea31e74877386434d
Jeff Gillis <jgillis@optoro.com>
2016-02-09 16:04:28 -0500

    Remove local consul service tests

--- test/integration/service/serverspec/default_spec.rb
+++ test/integration/service/serverspec/default_spec.rb
@@ -15,12 +15,4 @@ describe 'optoro_consul::server' do
     its(:content) { should match(/test_service/) }
     its(:content) { should match(/testing/) }
   end
-
-  describe command('curl http://localhost:8500/v1/catalog/service/test_service') do
-    its(:stdout) { should match(/test_service/) }
-  end
-
-  describe command('curl http://localhost:8500/v1/health/checks/test_service') do
-    its(:stdout) { should match(/passing/) }
-  end
 end
