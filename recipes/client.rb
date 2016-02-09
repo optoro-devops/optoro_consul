9a8cee5ed1ecd636126f4a3bbc657dc6ab2af5de
Jeff Gillis <jgillis@optoro.com>
2016-02-09 14:17:52 -0500

    Use consul application cookbook

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,7 +1,10 @@
 node.override['consul']['service_mode'] = 'client'
-node.default['consul']['serve_ui'] = false
-node.default['consul']['service']['server'] = false
-node.default['consul']['service']['expect_bootstrap'] = 0
+node.default['consul']['serve_ui'] = node['optoro_consul']['serve_ui']
+node.default['consul']['config']['enable_syslog'] = node['optoro_consul']['enable_syslog']
+node.default['consul']['config']['start_join'] = node['optoro_consul']['servers']
+node.default['consul']['config']['datacenter'] = node['optoro_consul']['datacenter']
+node.default['consul']['service']['config_dir'] = node['optoro_consul']['config_dir']
+
 include_recipe 'consul'
 
 logrotate_app 'consul_agent' do
