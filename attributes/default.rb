0042247b0ce9ec39beaec751ae4c740877b4fb53
Jeffrey Gillis <jgillis@optoro.com>
2016-05-20 15:16:18 -0400

    Update consul version to 0.6.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
-default['consul']['version'] = '0.6.3'
+default['consul']['version'] = '0.6.4'
 default['consul']['checksums'] = {
-  '0.6.3_linux_amd64' => 'b0532c61fec4a4f6d130c893fd8954ec007a6ad93effbe283a39224ed237e250',
-  '0.6.3_web_ui' => '93bbb300cacfe8de90fb3bd5ede7d37ae6ce014898edc520b9c96a676b2bbb72'
+  '0.6.4_linux_amd64' => 'abdf0e1856292468e2c9971420d73b805e93888e006c76324ae39416edcf0627',
+  '0.6.4_web_ui' => '5f8841b51e0e3e2eb1f1dc66a47310ae42b0448e77df14c83bb49e0e0d5fa4b7'
 }
 default['consul']['bootstrap_expect'] = '5'
 default['consul']['servers'] = [
