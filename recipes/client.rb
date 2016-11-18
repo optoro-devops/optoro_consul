3bb693ffd4eabae859fb37f61d928393344aee67
Scott Medefind <smedefind@optoro.com>
2016-11-18 10:49:32 -0500

    Moving consul service to use sysvinit instead of upstart

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,3 +1,18 @@
+# Force consul service to use sysvinit
+poise_service_options 'consul' do
+  provider 'sysvinit'
+end
+
+# Disable Upstart consul service
+service 'consul' do
+  provider Chef::Provider::Service::Upstart
+  action [:stop, :disable]
+end
+
+file '/etc/init/consul.conf' do
+  action :delete
+end
+
 include_recipe 'consul'
 
 logrotate_app 'consul_agent' do
