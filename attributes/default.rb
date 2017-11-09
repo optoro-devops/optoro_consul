803c114e513577cfbe9e9dce791643bba7e99a37
Jeffrey Gillis <jgillis@optoro.com>
2017-11-09 11:09:40 -0500

    Update server version to 1.0.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['consul']['client_address'] = '0.0.0.0'
-default['consul']['version'] = '0.6.4'
+default['consul']['version'] = '1.0.0'
 
 default['consul']['config']['enable_syslog'] = false
 
